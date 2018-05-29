3c7c43954cfb5d4702dfe2d700ae79eb8412cf7c
Lasha Lezhava <lashalezhava@gmail.com>
2018-05-29 17:29:21 +0400

    new test commits

--- src/CalculatorInputs.js
+++ src/CalculatorInputs.js
@@ -1,56 +1,59 @@
 import React, { Component } from "react";
-import {precisionRound} from "./LoanScheduleCalculator"
+import { precisionRound } from "./LoanScheduleCalculator";
 
 function CalculatorInputs(props) {
-    let inputs = props.inputs;
-  
-    function makeInput(key) {
-      let type = key
-      return (
-        <input
-          type={type}
-          value={precisionRound(inputs[key], 2)}
-          onChange={e => props.onUpdateInput(e, key)}
-          min = "0.1"
-        />
-      );
-    }
+  let inputs = props.inputs;
 
-    function checkIsSimple() {
-      if (!props.isSimple) {
-        return ( <form>
-          <label>
-          სესხის აღების თარიღი:
-          <input
-            type="date"
-            value={inputs.loanDate}
-            onChange={e => props.onUpdateDate(e)}
-          />
-        </label>
-        <label>გადახდის დღე: {makeInput("loanPayDay")}</label></form>)
-      }
-    }
-
-    function checkIfValid() {
-      if (!props.isValid) {
-        console.log("არასწორია")
-        return (props.errorMessage)
-      } else {
-      }
-    }
-  
+  function makeInput(key) {
+    let type = key;
     return (
-      <div className="CalculatorInputs">
-        <form>
-          <label>სესხის ოდენობა: {makeInput("loanAmount")}</label>
-          <label>ყოველთვიური გადასახადი: {makeInput("monthlyPayment")}</label>
-          <label>პროცენტი: {makeInput("interestRate")}</label>
-          <label>თვეების რაოდენობა: {makeInput("numberOfMonths")}</label>
-        </form>
-        {checkIsSimple()}
-        <button onClick={props.calculateOutput}>გამოთვლა</button>
-        {checkIfValid()}
-      </div>
+      <input
+        type={type}
+        value={precisionRound(inputs[key], 2)}
+        onChange={e => props.onUpdateInput(e, key)}
+        min="0.1"
+      />
     );
   }
+
+  function checkIsSimple() {
+    if (!props.isSimple) {
+      return (
+        <form>
+          <label>
+            სესხის აღების თარიღი:
+            <input
+              type="date"
+              value={inputs.loanDate}
+              onChange={e => props.onUpdateDate(e)}
+            />
+          </label>
+          <label>გადახდის დღე: {makeInput("loanPayDay")}</label>
+        </form>
+      );
+    }
+  }
+
+  function checkIfValid() {
+    if (!props.isValid) {
+      console.log("არასწორია");
+      return props.errorMessage;
+    } else {
+    }
+  }
+
+  return (
+    <div className="CalculatorInputs">
+      <form>
+        <label>სესხის ოდენობა: {makeInput("loanAmount")}</label>
+        <label>ყოველთვიური გადასახადი: {makeInput("monthlyPayment")}</label>
+        <label>პროცენტი: {makeInput("interestRate")}</label>
+        <label>თვეების რაოდენობა: {makeInput("numberOfMonths")}</label>
+      </form>
+      {checkIsSimple()}
+      <button onClick={props.calculateOutput}>გამოთვლა</button>
+      {checkIfValid()}
+    </div>
+  );
+}
 export default CalculatorInputs;
--- src/LoanScheduleCalculator.js
+++ src/LoanScheduleCalculator.js
@@ -37,7 +37,7 @@ const initialState = {
       totalInterest: 0
     }
   ],
-  isCalculatedFirst: false,
+  calculatedCount: 0,
   isValid: true,
   errorMessage: "აღნიშნული მონაცემებით სესხის დათვლა შეუძლებელია."
 };
@@ -48,20 +48,18 @@ class LoanScheduleCalculator extends Component {
     this.state = initialState;
   }
 
-
   reverseIsSimple = () => {
     this.setState({ isSimple: !this.state.isSimple });
     console.log("მარტივი");
   };
 
-  // Updates state when the date input is filled. 
+  // Updates state when the date input is filled.
   updateDate = e => {
     let value = e.target.value;
     let calculatorInputs = this.state.calculatorInputs;
     calculatorInputs.loanDate = value;
     this.setState({
-      calculatorInputs: calculatorInputs,
-      isCalculatedFirst: false
+      calculatorInputs: calculatorInputs
     });
   };
 
@@ -76,39 +74,141 @@ class LoanScheduleCalculator extends Component {
       }
     });
     this.setState({
-      calculatorInputs: currentCalculatorInputs,
-      isCalculatedFirst: false
+      calculatorInputs: currentCalculatorInputs
     });
   };
 
   // Creates table for the loan calculator
   calculateOutput = () => {
-    let that = this;
     let currentInputs = this.state.calculatorInputs;
+
+    // If the inputs are invalid, stop the execution
     if (!this.checkCurrentInputs(currentInputs, this.state.isSimple)) {
       this.setState({ calculatorOutput: initialState.calculatorOutput });
       return {};
     }
-    let monthlyPayment = this.getPMT();
-    let items = createTableItems(currentInputs.numberOfMonths);
 
-    function createTableItems(number) {
-      let array = [];
-      for (let i = 0; i < number; i++) {
-        array.push(9);
+    let firstOutput = this.calculateTheFirst();
+    let restOutput = this.calculateTheRest(
+      firstOutput,
+      this.state.calculatorInputs.numberOfMonths
+    );
+
+    console.log("The Rest Output is: ");
+    console.log(restOutput);
+
+    this.setState({
+      calculatorOutput: restOutput
+    });
+  };
+
+  // Calculates the first output for the array
+  calculateTheFirst = () => {
+    let firstOutput = this.getNextItem(0, undefined, false);
+    return firstOutput;
+  };
+
+  // Returns the complete array of items for the calculator output.
+  calculateTheRest = (firstOutput, numberOfMonths) => {
+    let restOutput = [firstOutput];
+
+    // Add Items to the restOutput array
+    for (let i = 1; i < numberOfMonths; i++) {
+      let nextItem = this.getNextItem(i, restOutput[i - 1], true);
+      restOutput.push(nextItem);
+    }
+
+    return restOutput;
+  };
+
+  // Takes Current month number and the object generated previously with this function.
+  // Getnerates the next item for the calculator output.
+  getNextItem = (currentMonthNumber, previousMonth, isCalculatedFirst) => {
+    let currentInputs = this.state.calculatorInputs;
+
+    let nextPayDate = formatDate(
+      getNextPayDate(isCalculatedFirst, currentInputs)
+    );
+    let monthlyPayment = this.getPMT(currentInputs);
+    let month = currentMonthNumber + 1;
+    let startingBalance = previousMonth
+      ? previousMonth.endingBalance
+      : currentInputs.loanAmount;
+    let interest = startingBalance * currentInputs.interestRate / 12 / 100;
+    let principal = monthlyPayment - interest;
+    let endingBalance = startingBalance - principal;
+    let totalInterest = previousMonth
+      ? previousMonth.totalInterest + interest
+      : interest;
+
+    if (!isCalculatedFirst) {
+      let daysDifference = PayDateLoanDate();
+      // Total interest for days elapsed from the loan date.
+      let interestFromLoanDateToPayDay = interest * 12 / 365 * daysDifference;
+
+      interest += interestFromLoanDateToPayDay;
+      totalInterest += interestFromLoanDateToPayDay;
+      console.log(interestFromLoanDateToPayDay);
+    } else {
+    }
+
+    return {
+      month: month,
+      payDay: nextPayDate,
+      startingBalance: startingBalance,
+      interest: interest,
+      principal: principal,
+      endingBalance: endingBalance,
+      totalInterest: totalInterest
+    };
+
+    function PayDateLoanDate() {
+      let loanDate = new Date(Date.parse(currentInputs.loanDate));
+      let payDayx = new Date(Date.parse(nextPayDate));
+      let differenceInDays = (payDayx - loanDate) / (60 * 60 * 24 * 1000);
+      console.log(`PayDay - Loan Date: " + ${payDayx}
+      Current Loan Date: ${currentInputs.loanDate}, Date Object: ${loanDate}
+
+      Next Loan Date: ${nextPayDate}, Date Object: ${payDayx}
+
+      Difference between them: ${(payDayx - loanDate) / (60 * 60 * 24 * 1000)}
+
+      isCalculatedFirst:
+
+      `);
+
+      return differenceInDays;
+    }
+
+    function getNextPayDate(isCalculatedFirst, currentInputs) {
+      let loanDate = new Date(Date.parse(currentInputs.loanDate));
+      // Day of month loan is going to be paid
+      let loanPayDay = currentInputs.loanPayDay;
+      comparePayDateLoanDates();
+      loanDate.setDate(loanPayDay);
+      checkHolidays();
+      return loanDate;
+
+      // If pay day is less, than day of month the loan is given, increment month by 1
+      function comparePayDateLoanDates() {
+        if (!isCalculatedFirst && loanPayDay < loanDate.getDate()) {
+          loanDate.setMonth(loanDate.getMonth() + currentMonthNumber);
+        } else {
+          loanDate.setMonth(loanDate.getMonth() + currentMonthNumber);
+        }
       }
-      return array;
+
+      function checkHolidays() {
+        let day = loanDate.getDay();
+        switch (day) {
+          case 0:
+            loanDate.setDate(loanPayDay + 1);
+            break;
+          case 6:
+            loanDate.setDate(loanPayDay + 2);
+        }
+      }
     }
-
-    for (let i = 0; i < currentInputs.numberOfMonths; i++) {
-      items[i] = this.getNextItem(i, items[i - 1]);
-    }
-
-    console.log(items);
-
-    this.setState({ calculatorOutput: items });
-
-    return items;
   };
 
   // Checks if inputs are valid, returns boolean
@@ -129,7 +229,9 @@ class LoanScheduleCalculator extends Component {
         currentInputs.numberOfMonths > 0 &&
         currentInputs.numberOfMonths % 1 === 0;
       let checkDate =
-        currentInputs.loanPayDay > 0 && currentInputs.loanPayDay < 31 && currentInputs.loanPayDay % 1 === 0;
+        currentInputs.loanPayDay > 0 &&
+        currentInputs.loanPayDay < 31 &&
+        currentInputs.loanPayDay % 1 === 0;
 
       console.log(checkSimple, checkDate);
 
@@ -137,66 +239,7 @@ class LoanScheduleCalculator extends Component {
     }
   };
 
-  // Takes Current month number and the object generated previously with this function.
-  // Getnerates the next item for the calculator output.
-  getNextItem = (currentMonthNumber, previousMonth) => {
-    let currentInputs = this.state.calculatorInputs;
-    let monthlyPayment = this.getPMT();
-    let month = currentMonthNumber + 1;
-    let startingBalance = previousMonth
-      ? previousMonth.endingBalance
-      : currentInputs.loanAmount;
-    let interest = startingBalance * currentInputs.interestRate / 12 / 100;
-    let principal = monthlyPayment - interest;
-    let endingBalance = startingBalance - principal;
-    let totalInterest = previousMonth
-      ? previousMonth.totalInterest + interest
-      : interest;
-    let payDay = formatDate(getNextPayDay(this.state.isCalculatedFirst));
-    this.setState({ isCalculatedFirst: true });
-    return {
-      month: month,
-      payDay: payDay,
-      startingBalance: startingBalance,
-      interest: interest,
-      principal: principal,
-      endingBalance: endingBalance,
-      totalInterest: totalInterest
-    };
-
-    function getNextPayDay(isCalculatedFirst) {
-      let loanDate = new Date(Date.parse(currentInputs.loanDate));
-      // Day of month loan is going to be paid
-      let loanPayDay = currentInputs.loanPayDay;
-      checkIfCalculatedFirst();
-      loanDate.setDate(loanPayDay);
-      console.log(loanDate);
-      checkHolidays();
-      return loanDate;
-
-      function checkIfCalculatedFirst() {
-        if (!isCalculatedFirst && loanPayDay < loanDate.getDate()) {
-          loanDate.setMonth(loanDate.getMonth() + currentMonthNumber + 1);
-        } else {
-          loanDate.setMonth(loanDate.getMonth() + currentMonthNumber);
-        }
-      }
-
-      function checkHolidays() {
-        let day = loanDate.getDay();
-        switch (day) {
-          case 0:
-            loanDate.setDate(loanPayDay + 1);
-            break;
-          case 6:
-            loanDate.setDate(loanPayDay + 2);
-        }
-      }
-    }
-  };
-
-  getPMT = () => {
-    let currentInputs = this.state.calculatorInputs;
+  getPMT = (currentInputs) => {
     let monthlyPayment = -finance.PMT(
       currentInputs.interestRate / 12 / 100,
       currentInputs.numberOfMonths,
